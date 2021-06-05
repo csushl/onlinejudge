9e8c46b5a3eb00976a900b07f30cf33ab21b7f42
csushl <csushl@163.com>
2021-06-05 15:24:35 +0800

    修稿安装脚本

--- install-ubuntu18.04.sh
+++ install-ubuntu18.04.sh
@@ -10,13 +10,14 @@ cd /home/judge/
 wget -O hustoj.tar.gz https://github.com/csushl/onlinejudge/blob/master/hustoj.tar.gz
 tar xzf hustoj.tar.gz
 svn up src
+#svn co https://github.com/zhblue/hustoj/trunk/trunk/  src
 
 for pkg in "net-tools make flex g++ clang libmysqlclient-dev libmysql++-dev php-fpm nginx mysql-server php-mysql  php-common php-gd php-zip fp-compiler openjdk-11-jdk mono-devel php-mbstring php-xml php-curl php-intl php-xmlrpc php-soap"
 do
-    while ! apt-get install -y $pkg 
-    do
-        echo "Network fail, retry... you might want to change another apt source for install"
-    done
+	while ! apt-get install -y $pkg 
+	do
+		echo "Network fail, retry... you might want to change another apt source for install"
+	done
 done
 
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
@@ -30,8 +31,8 @@ cp src/install/judge.conf  /home/judge/etc
 chmod +x src/install/ans2out
 
 if grep "OJ_SHM_RUN=0" etc/judge.conf ; then
-    mkdir run0 run1 run2 run3
-    chown judge run0 run1 run2 run3
+	mkdir run0 run1 run2 run3
+	chown judge run0 run1 run2 run3
 fi
 
 sed -i "s/OJ_USER_NAME=root/OJ_USER_NAME=$USER/g" etc/judge.conf
@@ -53,26 +54,26 @@ chmod 750 -R src/web/.svn
 
 chown www-data src/web/upload data
 if grep "client_max_body_size" /etc/nginx/nginx.conf ; then 
-    echo "client_max_body_size already added" ;
+	echo "client_max_body_size already added" ;
 else
-    sed -i "s:include /etc/nginx/mime.types;:client_max_body_size    80m;\n\tinclude /etc/nginx/mime.types;:g" /etc/nginx/nginx.conf
+	sed -i "s:include /etc/nginx/mime.types;:client_max_body_size    80m;\n\tinclude /etc/nginx/mime.types;:g" /etc/nginx/nginx.conf
 fi
 
 mysql -h localhost -u$USER -p$PASSWORD < src/install/db.sql
 echo "insert into jol.privilege values('admin','administrator','true','N');"|mysql -h localhost -u$USER -p$PASSWORD 
 
 if grep "added by hustoj" /etc/nginx/sites-enabled/default ; then
-    echo "default site modified!"
+	echo "default site modified!"
 else
-    echo "modify the default site"
-    sed -i "s#root /var/www/html;#root /home/judge/src/web;#g" /etc/nginx/sites-enabled/default
-    sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
-    sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
-    sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
-    sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
-    sed -i "s:}#added by hustoj::g" /etc/nginx/sites-enabled/default
-    sed -i "s:php7.0:php7.2:g" /etc/nginx/sites-enabled/default
-    sed -i "s|# deny access to .htaccess files|}#added by hustoj\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
+	echo "modify the default site"
+	sed -i "s#root /var/www/html;#root /home/judge/src/web;#g" /etc/nginx/sites-enabled/default
+	sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
+	sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
+	sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
+	sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
+	sed -i "s:}#added by hustoj::g" /etc/nginx/sites-enabled/default
+	sed -i "s:php7.0:php7.2:g" /etc/nginx/sites-enabled/default
+	sed -i "s|# deny access to .htaccess files|}#added by hustoj\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
 fi
 /etc/init.d/nginx restart
 sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php/7.2/fpm/php.ini
@@ -90,16 +91,16 @@ cd src/core
 chmod +x ./make.sh
 ./make.sh
 if grep "/usr/bin/judged" /etc/rc.local ; then
-    echo "auto start judged added!"
+	echo "auto start judged added!"
 else
-    sed -i "s/exit 0//g" /etc/rc.local
-    echo "/usr/bin/judged" >> /etc/rc.local
-    echo "exit 0" >> /etc/rc.local
+	sed -i "s/exit 0//g" /etc/rc.local
+	echo "/usr/bin/judged" >> /etc/rc.local
+	echo "exit 0" >> /etc/rc.local
 fi
 if grep "bak.sh" /var/spool/cron/crontabs/root ; then
-    echo "auto backup added!"
+	echo "auto backup added!"
 else
-    crontab -l > conf && echo "1 0 * * * /home/judge/src/install/bak.sh" >> conf && crontab conf && rm -f conf
+	crontab -l > conf && echo "1 0 * * * /home/judge/src/install/bak.sh" >> conf && crontab conf && rm -f conf
 fi
 ln -s /usr/bin/mcs /usr/bin/gmcs
 
